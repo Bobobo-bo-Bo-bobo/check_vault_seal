fa816097afbd92e7be01842b3177228fb00ce714
Andreas Maus <maus@ypbind.de>
2019-01-26 16:52:18 +0100

    Fix usage string

--- src/check_vault_seal/usage.go
+++ src/check_vault_seal/usage.go
@@ -6,7 +6,7 @@ const help string = `check_vault_seal version 1.0.2
 Copyright (C) by Andreas Maus <maus@ypbind.de>
 This program comes with ABSOLUTELY NO WARRANTY.
 
-redfish-tool is distributed under the Terms of the GNU General
+check_vault_seal is distributed under the Terms of the GNU General
 Public License Version 3. (http://www.gnu.org/copyleft/gpl.html)
 
 Usage: check_vault_seal --addr <vault_address>
